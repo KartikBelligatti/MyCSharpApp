9870c873377d9f63903540300564de70b69c121f
KartikBelligatti <belligattikartik17@gmail.com>
2024-08-05 10:56:13 +0530

    commit

--- obj/Debug/net7.0/MyCSharpApp.AssemblyInfo.cs
+++ obj/Debug/net7.0/MyCSharpApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyCSharpApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+daae58682fc6c75cae8aaf186ba5d7de8d044186")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0ccf654620a29d65575855b97d60b3182f195355")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyCSharpApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyCSharpApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
